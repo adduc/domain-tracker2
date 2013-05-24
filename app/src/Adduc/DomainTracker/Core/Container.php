957ac88f2087b847fbec5a8804b163da1a13edbe
John Long <a@88k.us>
2013-05-24 00:01:54 -0500

    Fixing Container offsetSet not able to handle arrays properly.

--- app/src/Adduc/DomainTracker/Core/Container.php
+++ app/src/Adduc/DomainTracker/Core/Container.php
@@ -26,23 +26,27 @@ class Container implements \ArrayAccess {
         return $current_value;
     }
 
-    public function offsetSet($offset, $value = null) {
-        if(is_array($offset)) {
-            foreach($offset as $key => $value) {
-                $this->offsetSet($key, $value);
+    public function offsetSet($offset, $value) {
+        if(is_array($value)) {
+            foreach($value as $o2 => $v2) {
+                $o2 = $offset ? "{$offset}.{$o2}" : "{$o2}";
+                $this->offsetSet($o2, $v2);
             }
+            return;
         }
 
         $current_value = &$this->data;
         $path = explode('.', $offset);
 
         while($path) {
+
             $current_offset = trim(array_shift($path));
             switch(true) {
                 case !is_array($current_value):
-                case !isset($current_value[$current_offset]):
                     $current_value = array();
                     break;
+                case !isset($current_value[$current_offset]):
+                    $current_value[$current_offset] = array();
                 default:
                     $current_value = &$current_value[$current_offset];
             }
