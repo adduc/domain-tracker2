2769281eef5f429dece1f9fe80ac4b2db91df396
John Long <a@88k.us>
2013-05-24 10:23:41 -0500

    Reminder to check for valid user to see if we need to redirect to dashboard.

--- app/src/Adduc/DomainTracker/Controller/IndexController.php
+++ app/src/Adduc/DomainTracker/Controller/IndexController.php
@@ -5,7 +5,13 @@ namespace Adduc\DomainTracker\Controller;
 class IndexController extends Controller {
 
     public function indexAction() {
-
+        switch(true) {
+            case !isset($_COOKIE[$this->config['cookie_name']]):
+            case !$_COOKIE[$this->config['cookie_name']]:
+                break;
+            default:
+                // Check Login to see if we need to redirect.
+        }
     }
 
 }
