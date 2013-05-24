f2782069fd25e9a7d3ff8789f24fd011cd119340
John Long <a@88k.us>
2013-05-24 00:02:13 -0500

    Add default value for config->cookie_name

--- app/src/Adduc/DomainTracker/Core/Config.php
+++ app/src/Adduc/DomainTracker/Core/Config.php
@@ -4,6 +4,10 @@ namespace Adduc\DomainTracker\Core;
 
 class Config extends Container {
 
+    protected $data = array(
+        'cookie_name' => '_'
+    );
+
     public function __construct($file = null) {
         is_null($file) || $this->loadFile($file);
     }
@@ -14,8 +18,15 @@ class Config extends Container {
             $msg = "Config file doesn't exist or is not readable.";
             throw new Exception\Ex500($msg);
         }
+        $this->offsetSet(null, $data);
+        //$this->data = array_merge_recursive($this->data, $data);
+    }
 
-        $this->data = $data;
+    public function offsetGet($offset) {
+        switch($offset) {
+            default:
+                return parent::offsetGet($offset);
+        }
     }
 
 }
