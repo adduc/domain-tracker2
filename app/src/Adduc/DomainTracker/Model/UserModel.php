ec7be5d22cc6d165fba71e593fd375554b7b8514
John Long <a@88k.us>
2013-05-29 23:03:26 -0500

    Initial work on isLoggedIn

--- app/src/Adduc/DomainTracker/Model/UserModel.php
+++ app/src/Adduc/DomainTracker/Model/UserModel.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace Adduc\DomainTracker\Model;
+use Adduc\DomainTracker\Exception;
 
 class UserModel extends Model {
 
     public function isLoggedIn() {
+        $cookie = $this->config['cookie_name'];
+        if(!$cookie) {
+            throw new Exception\InvalidConfig();
+        } elseif(!isset($_COOKIE[$cookie]) || !$_COOKIE[$cookie]) {
+            return false;
+        }
 
+        return true;
     }
 
 }
